dc6dde420c1da70fc840707902158c4f56b3f67f
Jagadeesh Reddy <jagadeesh.learn@gmail.com>
2026-05-20 17:37:47 +0530

    fix: removed zone js dependency.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
-  ],
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withHashLocation())
+  ]
 };
